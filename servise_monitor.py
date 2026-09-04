988c57deecbea89fa22915ed42b60ed71ae30157
Alexandr <sankudrrr9808@gmail.com>
2026-09-04 18:34:50 +0700

    Add basic site availability check

--- servise_monitor.py
+++ servise_monitor.py
@@ -1,1 +1,14 @@
-print("Servise_monitor")
+import requests
+
+url = "https://example.com"
+try:
+  response = requests.get(url, timeout=5)
+
+  if 200 <= response.status_code < 300:
+    print("Сайт доступен")
+  else:
+    print("Сайт ответил с ошибкой")
+  print('HTTP-код:',response.status_code)
+except requests.RequestException as error:
+  print('НЕ удалось подключится к сайту')
+  print('Причина,', error)
