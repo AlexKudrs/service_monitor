5b7a182e94bcfff866665b557f8f79880a5ca6dc
Alexandr <sankudrrr9808@gmail.com>
2026-09-04 22:47:15 +0700

    Add multi-site monitoring and main entry point

--- servise_monitor.py
+++ servise_monitor.py
@@ -1,14 +1,58 @@
 import requests
+import time
 
-url = "https://example.com"
-try:
-  response = requests.get(url, timeout=5)
 
-  if 200 <= response.status_code < 300:
-    print("Сайт доступен")
+
+def check_site(url):
+  try:
+    start = time.perf_counter()
+    response = requests.get(url, timeout=5)
+    end = time.perf_counter()
+    response_time = end - start
+    result = {
+      "url": url,
+      "available": 200<= response.status_code < 300,
+      "status_code": response.status_code,
+      "response_time": response_time,
+      "error": None
+    }
+    return result
+
+  except requests.RequestException as error:
+    result = {
+    "url": url,
+    "available": False,
+    "status_code": None,
+    "response_time": None,
+    "error": str(error)
+    }
+    return result
+
+
+def display_result(result):
+  print(f"Сайт: {result['url']}")
+
+  if result['available']:
+    print("Статус: Доступен")
+    print(f"HTTP: {result['status_code']}")
+    print(f"Время ответа: {result['response_time']:.3f} сек.")
   else:
-    print("Сайт ответил с ошибкой")
-  print('HTTP-код:',response.status_code)
-except requests.RequestException as error:
-  print('НЕ удалось подключится к сайту')
-  print('Причина,', error)
+    print("Статус: недоступен")
+    print(f"Ошибка: {result['error']}")
+
+def main():
+        
+  sites = [
+      "https://example.com",
+      "https://python.org",
+      "https://github.com"
+  ]
+
+  
+  for site in sites:
+    result = check_site(site)
+    display_result(result)
+
+if __name__ == "__main__":
+  main()
+
